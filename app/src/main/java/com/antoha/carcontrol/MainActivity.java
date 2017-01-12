511f8e673e3c2adeec2f38ba911017cd9c1cb4c3
Artjoms <artjoms.alijevs@squaliocc.com>
2017-01-12 17:53:51 +0200

    If button down, send commands non stop

--- app/src/main/java/com/antoha/carcontrol/MainActivity.java
+++ app/src/main/java/com/antoha/carcontrol/MainActivity.java
@@ -1,8 +1,10 @@
 package com.antoha.carcontrol;
 
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,13 +16,28 @@ import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button forward;
-    Button backward;
-    Button right;
-    Button left;
-    EditText etIp;
-    EditText etPort;
-    EditText logger;
+    private Button forward;
+    private Button backward;
+    private Button right;
+    private Button left;
+    private EditText etIp;
+    private EditText etPort;
+    private EditText logger;
+
+    private boolean forwardPressed = false;
+    private boolean backwardPressed = false;
+    private boolean leftPressed = false;
+    private boolean rightPressed = false;
+    private Runnable runnable = null;
+
+    private final int SEND_COMMAND_TIMEOUT = 100; //ms
+
+    public interface Message {
+        String FORWARD = "FORWARD";
+        String BACKWARD = "BACKWARD";
+        String LEFT = "LEFT";
+        String RIGHT = "RIGHT";
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,10 +57,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setKeyListeners() {
-        forward.setOnClickListener(new ClickListener("FORWARD"));
-        backward.setOnClickListener(new ClickListener("BACKWARD"));
-        left.setOnClickListener(new ClickListener("LEFT"));
-        right.setOnClickListener(new ClickListener("RIGHT"));
+        forward.setOnTouchListener(new ToutchListener(Message.FORWARD));
+        backward.setOnTouchListener(new ToutchListener(Message.BACKWARD));
+        left.setOnTouchListener(new ToutchListener(Message.LEFT));
+        right.setOnTouchListener(new ToutchListener(Message.RIGHT));;
     }
 
 
@@ -58,6 +75,10 @@ public class MainActivity extends AppCompatActivity {
         logger.setText(text);
     }
 
+    /**
+     * Send string to server via socket
+     * @param msg
+     */
     private void sendCommand(final String msg) {
         final Handler mainHandler = new Handler(this.getMainLooper());
         Thread thread = new Thread(new Runnable() {
@@ -94,22 +115,134 @@ public class MainActivity extends AppCompatActivity {
         thread.start();
     }
 
-    class ClickListener implements View.OnClickListener {
-        String msg;
-        ClickListener(String msg){
-            this.msg = msg;
+    /**
+     * Run task on new thread until button is pressed
+     * @param msg - button command
+     */
+    private void startSendingCommands(final String msg) {
+        final Handler mainHandler = new Handler(this.getMainLooper());
+        switch(msg){
+            case Message.BACKWARD:
+                backwardPressed = true;
+                runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        while(backwardPressed){
+                            sendWithTimeout(mainHandler, msg);
+                        }
+                    }
+                };
+                break;
+            case Message.FORWARD:
+                forwardPressed = true;
+                runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        while(forwardPressed){
+                            sendWithTimeout(mainHandler, msg);
+                        }
+                    }
+                };
+                break;
+            case Message.LEFT:
+                leftPressed = true;
+                runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        while(leftPressed){
+                            sendWithTimeout(mainHandler, msg);
+                        }
+                    }
+                };
+                break;
+            case Message.RIGHT:
+                rightPressed = true;
+                runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        while(rightPressed){
+                            sendWithTimeout(mainHandler, msg);
+                        }
+                    }
+                };
+                break;
+        }
+        if(runnable != null) {
+            AsyncTask.execute(runnable);
         }
 
-        @Override
-        public void onClick(View v) {
-            if(isServerInputed()){
-                log("Button " +msg +" clicked");
-                sendCommand(msg);
-            } else{
-                log("Ip or port is empty");
-                Toast.makeText(MainActivity.this, "IP/Port error", Toast.LENGTH_SHORT).show();
-            }
+    }
+
+    /**
+     * Wait constatnt SEND_COMMAND_TIMEOUT time and try to send command
+     * @param mainHandler
+     * @param msg
+     */
+    private void sendWithTimeout(Handler mainHandler, final String msg){
+        try {
+            Thread.sleep(SEND_COMMAND_TIMEOUT);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        //Run log on main thread
+        Runnable myRunnable = new Runnable() {
+            @Override
+            public void run() {
+                log("Loop sending " +msg);
+            }
+        };
+        mainHandler.post(myRunnable);
+        sendCommand(msg);
+    }
+
+    /**
+     * Set pressed flug false, for button with command msg
+     * @param msg
+     */
+    private void stopSendingCommands(String msg){
+        switch(msg){
+            case Message.BACKWARD:
+                backwardPressed = false;
+                break;
+            case Message.FORWARD:
+                forwardPressed = false;
+                break;
+            case Message.LEFT:
+                leftPressed = false;
+                break;
+            case Message.RIGHT:
+                rightPressed = false;
+                break;
+        }
+    }
+
+    /**
+     * Button listener
+     * On button is down start sending and on release stop
+     */
+    class ToutchListener implements View.OnTouchListener {
+        String msg;
+        ToutchListener(String msg){
+            this.msg = msg;
+        }
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                if(isServerInputed()){
+                    log("Button " +msg +" ACTION_DOWN");
+                    startSendingCommands(msg);
+                } else{
+                    log("Ip or port is empty");
+                    Toast.makeText(MainActivity.this, "IP/Port error", Toast.LENGTH_SHORT).show();
+                }
+
+            } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                stopSendingCommands(msg);
+
+            }
+            return false;
+        }
+
     }
 
 }
